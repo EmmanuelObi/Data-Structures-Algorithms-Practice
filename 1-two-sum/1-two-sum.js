c70f728d7f88ce000e4bbf61c7db80ddc9e169ce
Emmanuel <63916312+EmmanuelObi@users.noreply.github.com>
2022-08-12 08:26:25 +0100

    Time: 91 ms (79.96%), Space: 43.2 MB (31.06%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  var map = new Map();
+
+  for(var i = 0; i<nums.length; i++) {
+      var num = nums[i];
+      if(map.get(num) === undefined) map.set(target-num, i);
+      else return [map.get(num), i];
+  }
+};
