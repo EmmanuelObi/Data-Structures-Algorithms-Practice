9a45caa695cfdb99525c44662eae62b4576eb8ae
Emmanuel <63916312+EmmanuelObi@users.noreply.github.com>
2022-09-25 20:51:23 +0100

    Time: 158 ms (27.27%), Space: 48.6 MB (55.94%) - LeetHub

---
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var frequencySort = function(s) {
+    
+    const frequencyStore = new Map();
+    let result = []
+    
+    for(let i = 0; i < s.length; i++) {
+        if(frequencyStore.has(s[i])){
+            frequencyStore.set(s[i], frequencyStore.get(s[i]) + 1)
+        }else {
+            frequencyStore.set(s[i], 1)
+        }
+    }
+    
+    const sortedStore = [...frequencyStore].sort((a, b) => b[1] - a[1])
+    
+    for(const [key, value] of sortedStore) {
+        for(let i = 0; i < value; i++) {
+            result.push(key)
+        }
+    }
+    
+    return result
+    
+};
