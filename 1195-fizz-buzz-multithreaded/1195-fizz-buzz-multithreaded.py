46f71f12bcac866993a63ae27fa0b554f7457e17
Emmanuel <63916312+EmmanuelObi@users.noreply.github.com>
2022-09-26 17:42:37 +0100

    Time: 73 ms (32.95%), Space: 14.6 MB (55.56%) - LeetHub

---
+++ 1195-fizz-buzz-multithreaded/1195-fizz-buzz-multithreaded.py
@@ -0,0 +1,54 @@
+import threading
+
+class FizzBuzz:
+    def __init__(self, n: int):
+        self.done = False        
+        self.n = n
+        self.fSem = threading.Semaphore(0)
+        self.bSem = threading.Semaphore(0)
+        self.fbSem = threading.Semaphore(0)
+        self.nSem = threading.Semaphore(1)
+
+    # printFizz() outputs "fizz"
+    def fizz(self, printFizz: 'Callable[[], None]') -> None:
+        while True:
+            self.fSem.acquire()
+            if self.done: break
+            printFizz()
+            self.nSem.release()
+
+    # printBuzz() outputs "buzz"
+    def buzz(self, printBuzz: 'Callable[[], None]') -> None:
+        while True:
+            self.bSem.acquire()
+            if self.done: break
+            printBuzz()
+            self.nSem.release()
+
+    # printFizzBuzz() outputs "fizzbuzz"
+    def fizzbuzz(self, printFizzBuzz: 'Callable[[], None]') -> None:
+        while True:
+            self.fbSem.acquire()
+            if self.done: break
+            printFizzBuzz()
+            self.nSem.release()
+
+    # printNumber(x) outputs "x", where x is an integer.
+    def number(self, printNumber: 'Callable[[int], None]') -> None:
+        for i in range(1, self.n+1):
+            self.nSem.acquire()
+            if i % 15 == 0:
+                self.fbSem.release()
+            elif i % 3 == 0:
+                self.fSem.release()
+            elif i % 5 == 0:
+                self.bSem.release()
+            else:
+                printNumber(i)
+                self.nSem.release()
+        self.nSem.acquire() # needed so this thread waits to set done flag only when other threads are done with printing
+        self.done = True
+        self.fSem.release()  # let all other threads to break and finish
+        self.bSem.release()  #
+        self.fbSem.release() #
+        
