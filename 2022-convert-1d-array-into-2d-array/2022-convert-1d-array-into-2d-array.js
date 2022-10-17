eec288d99e2ebc1d1b3f71e35f7cf4d1df8660ce
Emmanuel <63916312+EmmanuelObi@users.noreply.github.com>
2022-10-17 08:46:02 +0100

    Time: 240 ms (91.04%), Space: 62 MB (90.15%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.js
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.js
@@ -5,22 +5,29 @@
  * @return {number[][]}
  */
 var construct2DArray = function(original, m, n) {
-    let store = [];
-    let miniArr = [];
+//     let store = [];
+//     let miniArr = [];
     
-    if(m * n === original.length) {
+//     if(m * n === original.length) {
         
-        for(let i = 0; i < original.length; i++) {
+//         for(let i = 0; i < original.length; i++) {
             
-            miniArr.push(original[i])
-            if(miniArr.length === n){
-                store.push(miniArr);
-                miniArr = []
-            }
-        }
+//             miniArr.push(original[i])
+//             if(miniArr.length === n){
+//                 store.push(miniArr);
+//                 miniArr = []
+//             }
+//         }
         
-        return store
-    }
+//         return store
+//     }
     
-    return []   
+//     return []
+      if(original.length != m*n) return []
+    let result = Array.from(Array(m), () => new Array(n));
+    
+    for(let i = 0; i < m*n; i++) {
+        result[Math.floor(i/n)][i%n] = original[i];
+    }
+    return result
 };
